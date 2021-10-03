bf6c2d573cbd05731bdaa1ef58021d63781310b3
BibhushanKarki <bibhushan.karki11@gmail.com>
2021-10-03 22:49:44 +0545

    login-page

---
+++ src/context/AuthReducer.js
@@ -0,0 +1,29 @@
+const AuthReducer=(state,action)=>{
+    switch(action.type){
+        case 'LOGIN_START':
+            return{
+                ...state,
+                user:null,
+                isFetching:false,
+                erorr:false,
+            };
+
+            case 'LOGIN_SUCCESS':
+            return{
+                user:action.payload,
+                isFetching:false,
+                erorr:false,
+            };
+
+            case 'LOGIN_FAILURE':
+            return{
+                user:null,
+                isFetching:false,
+                erorr:action.payload,
+            };
+
+            default:
+            return state;
+    }
+};
+export default AuthReducer;
